f3a9a2ae938f558ad70a4acb6707f0b08783ff72
Adam <adammoore118@outlook.com>
2022-03-30 16:42:33 -0400

    intro

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
+import React from 'react';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='d-flex flex-md-column flex-row flex-shrink-0 bg-light navigation align-items-center'></div>
+      <div className='intro-container'>
+        <div id="name">
+        <h1>Adam Moore</h1>
+        </div>
+      <div id="intro">
+      <p>I'm Adam, a full-stack engineer who loves problem solving. After dabbling in code for several years, I decided to enroll in Flatiron School's Software Engineering program in order to enhance my skills and prepare for the next step in my career.</p>
+      <br></br>
+      <p>Previously, I worked as a firefighter and EMT where I earned multiple certifications; never letting a chance to enhance my skill set and broaden my knowledge pass me by. It was in this role that I developed the communication and time management skills that are essential when working within a team at a high-stress level. Along with my professional career, I owned and operated a small landscaping business with forty-five individual clients and two employees. This experience allowed me to sharpen my leadership and organizational skills. By taking advantage of these experiences, I was able to devote more time to my passion for technology, leading to my dream of transitioning to full-time web development.</p>
+      <br></br>
+      <p>As a recent graduate of this software engineering course, I am eager to learn more about other frameworks and languages as well as sharpen the skills that I gained as a result of the course. If you are looking for a passionate, focused, and efficient full-stack engineer that is eager to take on any challenge, then please reach out to me.</p>
+      </div>
+      <div className='skill-set'>
+
+      </div>
+     
+      </div>
+
     </div>
   );
 }
